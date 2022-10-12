4d369048d7ff06b9108b892099b99110edbc61cf
Alexander Dolgosheev <izumroot@deeplay.io>
2022-10-13 02:07:15 +0600

    Test gRPC Service Mock

---
+++ Test.Grpc.Mock/Program.cs
@@ -0,0 +1,21 @@
+﻿
+using Grpc.Net.Client;
+
+using TestGrpc;
+
+using var channel = GrpcChannel.ForAddress("http://localhost:30001");
+var client = new TestGrpcService.TestGrpcServiceClient(channel);
+
+var getUser = client.GetUserAsync(new GetUserRequest
+{
+    UserId = 1
+}, deadline: DateTime.UtcNow.AddMinutes(1));
+
+Console.WriteLine($"User 1 is :\r\n{await getUser}");
+
+var getUsers = client.GetUsersAsync(new GetUsersRequest(), deadline: DateTime.UtcNow.AddMinutes(1));
+
+Console.WriteLine($"All Users List 1 is :\r\n{await getUsers}");
+
+Console.WriteLine("Press any key to exit...");
+Console.ReadKey();
